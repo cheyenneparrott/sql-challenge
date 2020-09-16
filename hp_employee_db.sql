b6274a2eebfae8f34489215a60dbd762e10e0096
Cheyenne Parrott <cdorsey2@ku.edu>
2020-09-16 14:33:25 -0500

    Adding HP Employee DB SQL file

---
+++ hp_employee_db.sql
@@ -0,0 +1,34 @@
+-- CREATE TABLE departments (
+-- 	dept_no VARCHAR NOT NULL,
+-- 	dept_name VARCHAR NOT NULL
+-- );
+ 
+-- CREATE TABLE dept_emp (
+-- 	dept_no VARCHAR NOT NULL,
+-- 	emp_no VARCHAR NOT NULL
+-- );
+
+-- CREATE TABLE dept_manager (
+-- 	dept_no VARCHAR NOT NULL,
+-- 	emp_no VARCHAR NOT NULL
+-- );
+
+-- CREATE TABLE employees (
+-- 	emp_no VARCHAR NOT NULL,
+-- 	emp_title_id VARCHAR NOT NULL,
+-- 	birth_date DATE NOT NULL,
+-- 	first_name VARCHAR NOT NULL,
+-- 	last_name VARCHAR NOT NULL,
+-- 	sex VARCHAR NOT NULL,
+-- 	hire_date DATE NOT NULL
+-- );
+
+-- CREATE TABLE salaries (
+-- 	emp_no VARCHAR NOT NULL,
+-- 	salary VARCHAR NOT NULL
+-- );
+
+-- CREATE TABLE titles (
+-- 	title_id VARCHAR NOT NULL,
+-- 	title VARCHAR NOT NULL
+-- );
