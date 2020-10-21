a5c1149b1fbd691756b4e4436c38f0c646266eca
Cheyenne Parrott <cdorsey2@ku.edu>
2020-10-21 18:58:35 -0500

    Adding updated SQL files.

--- queries.sql
+++ queries.sql
@@ -1,27 +1,23 @@
--- List the following details of each employee: employee number, last name, first name, sex, and salary.
+-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 from employees as e join salaries as s
 on e.emp_no = s.emp_no
 
--- List first name, last name, and hire date for employees who were hired in 1986.
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
 select first_name, last_name, hire_date
 from employees
 where hire_date between '1986-01-01' and '1986-12-31'
 
--- List the manager of each department with the following information: department number, department name,
--- the manager's employee number, last name, first name.
--- tables: dept_manager, departments, employees
-
+-- 3. List the manager of each department with the following information: 
+-- department number, department name, the manager's employee number, last name, first name.
 select d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
 from departments as d join dept_manager as dm 
 on d.dept_no = dm.dept_no
 join employees as e
 on dm.emp_no = e.emp_no
 
---#4 List the department of each employee with the following information: employee number, last name, first name, and department name.
--- tables: departments, dept_emp, employees,
--- columns: dept_name, emp_no, last_name, first_name
--- COME BACK TO THIS LATER
+--#4 List the department of each employee with the following information:
+-- employee number, last name, first name, and department name.
 select de.emp_no, e.last_name, e.first_name, d.dept_name
 from employees as e join dept_emp as de
 on e.emp_no = de.emp_no
@@ -33,12 +29,14 @@ select * from departments
 select * from dept_emp
 select * from dept_manager
 
--- #5 List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+-- 5.  List first name, last name, and sex for employees whose first name
+-- is "Hercules" and last names begin with "B."
 select first_name, last_name, sex
 from employees
 where first_name = 'Hercules' and last_name like 'B%'
 
--- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+-- 6. List all employees in the Sales department, including their 
+-- employee number, last name, first name, and department name.
 select de.emp_no, e.last_name, e.first_name, d.dept_name
 from dept_emp as de
 join employees as e
@@ -47,7 +45,8 @@ join departments as d
 on de.dept_no = d.dept_no
 where d.dept_name = 'Sales'
 
--- 6. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+-- 6. List all employees in the Sales and Development departments, 
+-- including their employee number, last name, first name, and department name.
 select de.emp_no, e.last_name, e.first_name, d.dept_name
 from dept_emp as de
 join employees as e
@@ -55,7 +54,6 @@ on de.emp_no=e.emp_no
 join departments as d
 on de.dept_no = d.dept_no
 where d.dept_name = 'Sales' OR d.dept_name = 'Development'
-
 
 ---------- troubleshoot------------
 select dm.emp_no, e.last_name, e.first_name
@@ -70,10 +68,3 @@ select  last_name,count(last_name)
 from employees
 group by last_name
 order by 2 desc
-
-
-
-
-
-
-
